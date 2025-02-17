81b0acd3e981e488e878844f91e7a8ae024d630d
DarkOwl490 <darkowl490@gmail.com>
2025-02-17 04:19:34 -0500

    Add random message retrieval functions for title, loading, and death screens

--- mixedMessages.js
+++ mixedMessages.js
@@ -3,3 +3,20 @@ const message_storage = {
     'Loading Screen': ['Loading...', 'Please wait...', 'Initializing game...', 'Please be patient...', 'Just a few seconds...', 'Loading world...', 'Don\'t dig straight down...', 'Use knockback against creepers.', 'Watch out for lava.', 'Use a bed to set your spawn point', 'Use a bucket of water to save yourself from falling.', 'A full set of armour uses 24 iron ingots.', 'Blocks break on the northmost face.', 'Evokers can change the wool color of sheep.', 'Mushroom biomes are safe.', 'Creepers are scared of cats.', 'Milking a mooshroom gives you mushroom stew.', 'Build a crafting table and pickaxe first to gather stone before anything else.'],
     'Death Screen': ['You died!', 'Game over!', 'You have perished!', 'You have been slain!', 'You are dead!', 'You have met your demise!', 'You have been defeated!', 'You have fallen!', 'You have succumbed to the void!', 'You have been vanquished!', 'You have been eliminated!', 'You have been crushed!', 'You have been obliterated!', 'You have been terminated!', 'You have been dispatched!', 'You have been extinguished!', 'Wah wah wahhh!', 'Death by snu snu!']
 }
+// This function returns a random message from the specified screen category
+const getRandomMessage = (screen) => {
+    if (message_storage[screen]) {
+        const messages = message_storage[screen];
+        const randomIndex = Math.floor(Math.random() * messages.length);
+        return messages[randomIndex];
+    } else {
+        console.error(`No messages found for screen: ${screen}`);
+        return null;
+    }
+}
+const getRandomTitleMessage = () => getRandomMessage('Title Screen');
+// console.log(getRandomTitleMessage());
+const getRandomLoadingMessage = () => getRandomMessage('Loading Screen');
+// console.log(getRandomLoadingMessage());
+const getRandomDeathMessage = () => getRandomMessage('Death Screen');
+// console.log(getRandomDeathMessage());
